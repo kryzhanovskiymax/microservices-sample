2e5d8face64757447411f40cd9216418d9c83865
Max Kryzhanovskiy <kryzhanovskiymax@mail.ru>
2023-07-03 20:53:37 +0300

    some stable changes added

---
+++ logic/User.js
@@ -0,0 +1,7 @@
+const { Schema, model } = require('mongoose');
+
+const User = new Schema({
+    login: { type: String, required: true }
+});
+
+module.exports = model('User',  User);
--- logic/index.js
+++ logic/index.js
@@ -1,18 +1,37 @@
 const amqplib = require('amqplib');
 const { request, reply } = require('amqplib-rpc');
+const mongoose = require('mongoose');
+const User = require('./User');
+const config = require('config');
 
 const PORT = 8484;
 const QUEUE_NAME = 'link';
 
 const start = async () => {
     try {
+        const clusterUrl = config.get('DATABASE_URL');
+        mongoose.set('strictQuery', true);
+        await mongoose.connect(clusterUrl, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+
+        console.log('connected to databse succesfully');
+
         queue = QUEUE_NAME;
-        const connection = await amqplib.connect('amqp://localhost');
+        const connection = await amqplib.connect(config.get('AMQP_URL'));
         const consumerChannel = await connection.createChannel();
         const publisherChannel = await connection.createChannel();
         console.log('server is started');
         consumerChannel.consume(QUEUE_NAME, async (msg) => {
-            console.log(msg.content.toString());
+            const user = msg.content.toString();
+            const exists = await User.findOne({login: user});
+            if (!exists) {
+                const newUser = new User({ login: user });
+                await newUser.save();
+                console.log('User added to DataBase');
+            }
+            console.log(user);
             reply(publisherChannel, msg, 'Hello, ' + msg.content.toString(), {});
             consumerChannel.ack(msg)
         });
